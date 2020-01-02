406216ecd149e0340d846094034762b99646c5dd
Young Cha <youngc0905@gmail.com>
2020-01-02 15:02:05 -0800

    implemented mongo seeding

--- database/MongoDB/createMongo.js
+++ database/MongoDB/createMongo.js
@@ -1,26 +1,73 @@
-// **MONGO SCHEMA**
+const mongoose = require('mongoose');
+const mongoUri = 'mongodb://127.0.0.1:27017/restaurants'
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoUri);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+});
+const Schema = mongoose.Schema;
+const seedMongo = require('./mongoSeed.js');
 
-let commentorSchema = new Schema({
-  user_name: String,
-  first_name: String,
-  last_name: String,
-  reviews: Number,
-  avatar_url: String
-})
-
-let imageSchema = new Schema({
+let imagesSchema = new Schema({
+  id : Number,
   image_url: String,
   helpful: Number,
   not_helpful: Number,
-  date: Date,
-  cuisine: String,
-  commentor: [commentorSchema]
-})
+  created_at: String,
+  caption: String,
+  restaurant: String,
+  user:
+  {
+    user_name: String,
+    first_name: String,
+    last_name: String,
+    stars: Number,
+    reviews: Number,
+    avatar_url: String,
+    elite_status: Boolean
+  }
+});
 
-let resterauntSchema = new Schema({
-  name: String,
-  genre: String,
-  images: [imageSchema],
-  image_count: Number
-})
+
+// let restaurantsSchema = new Schema({
+//   id: Number,
+//   name: String,
+//   image_count: Number,
+//   images: [
+//     {
+//       image_url: String,
+//       helpful: Number,
+//       not_helpful: Number,
+//       created_at: String,
+//       caption: String,
+//       user:
+//         {
+//           user_name: String,
+//           first_name: String,
+//           last_name: String,
+//           stars: Number,
+//           reviews: Number,
+//           avatar_url: String,
+//           elite_status: Boolean
+//         }
+//     }
+//   ]
+// });
+
+const Images = mongoose.model('Images', imagesSchema);
+
+// const Restaurants = mongoose.model('Restaurants', restaurantsSchema);
+
+// const insertData = () => {
+//   Restaurants.create(seedMongo())
+//     .then(() => console.log('completed seeding'));
+// };
+const insertData = () => {
+  Images.create(seedMongo())
+    .then(() => console.log('completed seeding'));
+};
+
+insertData();
+
 
