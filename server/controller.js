f12852a2d280765c2efa347c565bf6e2c4715ab3
Young Cha <youngc0905@gmail.com>
2020-01-04 10:47:34 -0800

    implemented client side code to use new db, implemented artillery testing

--- server/controller.js
+++ server/controller.js
@@ -1,11 +1,17 @@
-const db = require('../database/index.js');
+// const db = require('../database/index.js');
 const model = require('./model');
 
 module.exports = {
 
-  getImage: (id) =>{
+  getImage: (id, res) =>{
 
-    return Promise.resolve(model.getImage(id));
+    model.getImage(id, (err, data) => {
+      if (err) {
+        res.status(400).send('error getting data from database');
+      } else {
+        res.status(200).send(data);
+      }
+    })
 
   }
 
--- server/model.js
+++ server/model.js
@@ -1,10 +1,22 @@
 const express = require ('express');
-const db = require('../database/index.js');
-
+// const db = require('../database/index.js');
+const { Pool } = require('pg');
+let pool = new Pool({
+  database: 'carousel',
+});
 module.exports = {
-
-  getImage: (id) => db.Images.findAll({
-    where: id
-  })
-
+  getImage: (id, callback) => {
+    (async () => {
+      console.log(id)
+      const query = await pool.query(`SELECT image_url FROM images WHERE restaurant_id=${id};`)
+      .then((data) => {
+        console.log(data)
+        callback(null, data)
+      })
+      .catch((err) => {
+        console.log(`error with query: ${err}`)
+        callback(err, null)
+      })
+    })();
+  }
 };
